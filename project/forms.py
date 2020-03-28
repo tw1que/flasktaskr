38d42f2a820cccf6a8e9f29a4f07fa5e0523b2ca
Jim Stoop <jim.stoop@me.com>
2020-03-28 17:15:58 +0100

    final form

---
+++ project/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import Form
+from wtforms import StringField, DateField, IntegerField, \
+					SelectField
+from wtforms.validators import DataRequired
+
+class AddTaskForm(Form):
+	task_id = IntegerField()
+	name = StringField(
+		'TaskName', 
+		validators=[DataRequired()]
+		)
+	due_date = DateField(
+		'Date Due (mm/dd/yyyy)',
+		validators=[DataRequired()],
+		format='%m/%d/%y'
+		)
+	priority = SelectField(
+		'Priority',
+		validators=[DataRequired()],
+		choices=[
+			('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
+			('6', '6'), ('6', '6'), ('8', '8'), ('9', '9'), ('10','10')
+			]
+		)
+	status = IntegerField('Status')
--- project/views.py
+++ project/views.py
@@ -2,7 +2,8 @@ import sqlite3
 from functools import wraps
 
 from flask import Flask, flash, redirect, render_template, \
-								request, session, url_for
+								request, session, url_for, g
+from forms import AddTaskForm
 
 
 # config
@@ -40,7 +41,7 @@ def logout():
 def login():
 	if request.method=='POST':
 		if request.form['username'] != app.config['USERNAME'] or \
-			request.form['passowrd'] != app.config['PASSWORD']:
+			request.form['password'] != app.config['PASSWORD']:
 			error = 'Invalid Credentials. Please try again'
 			return render_template('login.html', error=error)
 		else:
@@ -49,5 +50,94 @@ def login():
 			return redirect(url_for('tasks'))
 
 	return render_template('login.html')
+
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+	with connect_db() as g.db:
+		cursor = g.db.execute(
+			'SELECT name, due_date, priority, task_id FROM tasks WHERE status=1'
+			)
+
+		open_tasks = [
+			dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3])
+				for row in cursor.fetchall()
+		]
+
+		cursor = g.db.execute(
+			'SELECT name, due_date, priority, task_id FROM tasks WHERE status=0'
+			)
+
+		closed_tasks = [
+			dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3])
+				for row in cursor.fetchall()
+		]
+
+	return render_template(
+		'tasks.html', 
+		form=AddTaskForm(request.form),
+		open_tasks=open_tasks,
+		closed_tasks=closed_tasks
+		)
+
+
+#add new task
+@app.route('/add/', methods=['POST'])
+@login_required
+def new_task():
+	with connect_db() as g.db:
+
+		name = request.form['name']
+		date = request.form['due_date']
+		priority = request.form['priority']
+		if not name or not date or not priority:
+			flash('All fields are required. Please try agian.')
+			return redirect(url_for('tasks'))
+		else:
+			g.db.execute('INSERT INTO tasks (name, due_date, priority, \
+							status) VALUES (?,?,?,1)', [
+												request.form['name'],
+												request.form['due_date'],
+												request.form['priority']
+												]
+												)
+			flash('New entry was succesfully posted')
+	
+	return redirect(url_for('tasks'))
+
+
+# mark tasks as complete
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+	with connect_db() as g.db:
+		g.db.execute('UPDATE tasks SET status = 0 WHERE task_id = ?', 
+						(task_id, ))
+
+		flash('The task was marked as comlete.')
+
+	return redirect(url_for('tasks'))
+
+
+# delete tasks
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+	with connect_db() as g.db:
+		g.db.execute('DELETE FROM tasks WHERE task_id = ?', (task_id, ))
+
+		flash('Task was deleted succesfully')
+
+	return redirect(url_for('tasks'))
+
+
+
+
+
+
+
+
+
 
 
